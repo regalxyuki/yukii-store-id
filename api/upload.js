3843da9970720a57ffbffafb2c0312da9b9632ab
regalxyuki <regalcoolbet@gmail.com>
2026-08-18 13:34:19 +0800

    Add files via upload

--- api/upload.js
+++ api/upload.js
@@ -32,7 +32,7 @@ export default async function handler(req, res) {
 
         return res.status(200).json({ 
             success: true, 
-            download_url: `https://yukii-store.vercel.app/${fileName}` 
+            download_url: `https://yukii-store-id.vercel.app/${fileName}` 
         });
     } catch (error) {
         return res.status(500).json({ error: error.message });
